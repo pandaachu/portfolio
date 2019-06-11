00b55eadaf6ef263c0374aa91339b61cca832afe
pandaachu <pandaachu@gmail.com>
2019-06-11 23:26:23 +0800

    parallax test

--- common/js/main.js
+++ common/js/main.js
@@ -1,29 +1,25 @@
-$(document).ready(function() {
-    
-    /* Every time the window is scrolled ... */
-    $(window).scroll( function(){
-    
-        /* Check the location of each desired element */
-        $('.heading,h2,h3,p,tr').each( function(i){
-            
-            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
-            
-            /* If the object is completely visible in the window, fade it it */
-            if( bottom_of_window > bottom_of_object ){
-                
-                $(this).animate({'opacity':'1'},2000);
-                    
-            }
-            
-        }); 
-    
-    });
-    
-});
-
 
 $(function () {
+
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+
+        /* Check the location of each desired element */
+        $('.heading,h2,h3,p,tr').each(function (i) {
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object) {
+
+                $(this).animate({ 'opacity': '1' }, 2000);
+
+            }
+
+        });
+
+    });
 
     /*
      * Slideshow
@@ -40,11 +36,11 @@ $(function () {
         // 每7500毫秒就執行showNextSlide函數
         setInterval(showNextSlide, 4500);
 
-          // 顯示下一張Slide的函數
-        function showNextSlide () {
+        // 顯示下一張Slide的函數
+        function showNextSlide() {
 
-						//下張Slide的index
-						//(如果是最後一張Slide，則會到第一張)
+            //下張Slide的index
+            //(如果是最後一張Slide，則會到第一張)
             var nextIndex = (currentIndex + 1) % slideCount;
 
             // 目前的Slide淡出顯示
@@ -53,19 +49,32 @@ $(function () {
             // 下一張Slide淡入顯示
             $slides.eq(nextIndex).fadeIn();
 
-               // 更新目前的index
+            // 更新目前的index
             currentIndex = nextIndex;
         }
 
     });
 
+    // Wrap every letter in a span
+    $('.ml6 .letters').each(function () {
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline({ loop: true })
+        .add({
+            targets: '.ml6 .letter',
+            translateY: ["1.1em", 0],
+            translateZ: 0,
+            duration: 750,
+            delay: function (el, i) {
+                return 50 * i;
+            }
+        }).add({
+            targets: '.ml6',
+            opacity: 0,
+            duration: 1000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
 });
-
-
-//
-//$.scrollingParallax('../img/11-banner/banner-1.jpg');
-//
-//$.scrollingParallax('../img/3-calendar/calendar_bg.png');
-//
-//$.scrollingParallax('../img/11-banner/banner-1.jpg');
 
