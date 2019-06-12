b889ffe07ee691cea44b9e68c52aecd3c5a1b827
pandaachu <pandaachu@gmail.com>
2019-06-12 17:24:33 +0800

    heading
    heading animation

--- common/js/main.js
+++ common/js/main.js
@@ -62,10 +62,25 @@ $(function () {
 });
 
 
-//
-//$.scrollingParallax('../img/11-banner/banner-1.jpg');
-//
-//$.scrollingParallax('../img/3-calendar/calendar_bg.png');
-//
-//$.scrollingParallax('../img/11-banner/banner-1.jpg');
+// Wrap every letter in a span
+$('.ml6 .letters').each(function(){
+    $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+  });
+  
+  anime.timeline({loop: true})
+    .add({
+      targets: '.ml6 .letter',
+      translateY: ["1.1em", 0],
+      translateZ: 0,
+      duration: 750,
+      delay: function(el, i) {
+        return 50 * i;
+      }
+    }).add({
+      targets: '.ml6',
+      opacity: 0,
+      duration: 1000,
+      easing: "easeOutExpo",
+      delay: 1000
+    });
 
