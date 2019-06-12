112ce8bd7fb5ec117527f931a19f9340a528811b
pandaachu <pandaachu@gmail.com>
2019-06-12 23:11:55 +0800

    刪註解

--- common/js/main.js
+++ common/js/main.js
@@ -1,16 +1,13 @@
 
 $(function () {
 
-  /* Every time the window is scrolled ... */
   $(window).scroll(function () {
 
-      /* Check the location of each desired element */
       $('.heading,h2,h3,p,tr').each(function (i) {
 
           var bottom_of_object = $(this).offset().top + $(this).outerHeight();
           var bottom_of_window = $(window).scrollTop() + $(window).height();
 
-          /* If the object is completely visible in the window, fade it it */
           if (bottom_of_window > bottom_of_object) {
 
               $(this).animate({ 'opacity': '1' }, 2000);
@@ -21,41 +18,31 @@ $(function () {
 
   });
 
-  /*
-   * Slideshow
-   */
-  // 對每個有slideshow類別的元素進行處理
+  
   $('.slideshow').each(function () {
 
-      var $slides = $(this).find('img'), // 所有Slide
-          slideCount = $slides.length,   // Slide個數
-          currentIndex = 0;              // 目前Slide的index
-      // 淡入顯示首張Slide
+      var $slides = $(this).find('img'), 
+          slideCount = $slides.length,   
+          currentIndex = 0;              
+   
       $slides.eq(currentIndex).fadeIn();
 
-      // 每7500毫秒就執行showNextSlide函數
       setInterval(showNextSlide, 4500);
 
-      // 顯示下一張Slide的函數
       function showNextSlide() {
 
-          //下張Slide的index
-          //(如果是最後一張Slide，則會到第一張)
           var nextIndex = (currentIndex + 1) % slideCount;
 
-          // 目前的Slide淡出顯示
           $slides.eq(currentIndex).fadeOut();
 
-          // 下一張Slide淡入顯示
           $slides.eq(nextIndex).fadeIn();
 
-          // 更新目前的index
           currentIndex = nextIndex;
       }
 
   });
 
-  // Wrap every letter in a span
+  
   $('.ml6 .letters').each(function () {
       $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
   });
